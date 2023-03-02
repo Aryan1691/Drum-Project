6e6590ad142020da90cfab8a51011cedd870a80c
Aryan Khatik <rkhatik156@gmail.com>
2023-03-02 20:50:06 +0530

    new commit

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-// window.addEventListener('keydown',run);
+window.addEventListener('keydown',run);
 
-// function run(e){
-// const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-// if(!audio) return; 
+function run(e){
+const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+if(!audio) return; 
 
-// audio.play();
+audio.play();
 
-// }
-	const person = {
-    firstName:"John",
-    lastName: "Doe",
-    fullName: function () {
-      return this.firstName + " " + this.lastName;
-    }
-  }
-  
-  const member = {
-    firstName:"Hege",
-    lastName: "Nilsen",
-  }
-  
-  let fullName = person.fullName.bind(member);
-console.log  (member.fullName);
+}
